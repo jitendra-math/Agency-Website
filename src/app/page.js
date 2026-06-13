9875807c0e724b549daf80c24cad3445483c82a3
Jitendra Singh <i.jitendra.singh0@gmail.com>
2026-06-13 15:49:15 +0530

    Updates & Deletions via Repo Manager 🚀

--- src/app/page.js
+++ src/app/page.js
@@ -6,21 +6,29 @@ import ServicesSection from "@/components/sections/ServicesSection";
 import WhyChooseSection from "@/components/sections/WhyChooseSection";
 // import ProjectsPreview from "@/components/sections/ProjectsPreview";
 import PricingPlans from "@/components/sections/work/PricingPlans";
-import JoinUsCTA from "@/components/sections/JoinUsCTA";
+// import JoinUsCTA from "@/components/sections/JoinUsCTA";
+import FinalCTA from "@/components/sections/FinalCTA";
 import ContactCTA from "@/components/sections/ContactCTA";
 
 export default function HomePage() {
   return (
     <>
       <HeroSection />
+
       <AboutPreview />
+
       <ServicesSection />
+
       <WhyChooseSection />
 
+      {/* Add this back once you have strong case studies */}
       {/* <ProjectsPreview /> */}
 
       <PricingPlans />
-      <JoinUsCTA />
+
+      {/* Final conversion-focused CTA */}
+      <FinalCTA />
+
       <ContactCTA />
     </>
   );
